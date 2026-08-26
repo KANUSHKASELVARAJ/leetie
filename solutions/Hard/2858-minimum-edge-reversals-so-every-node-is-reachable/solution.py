1463e5307cf98f103ffbad5b9bedf2dab758c6be
KANUSHKASELVARAJ <kanushkaselvaraj@users.noreply.github.com>
2026-08-26 08:23:52 +0000

    leetie: Add 2858. Minimum Edge Reversals So Every Node Is Reachable [Hard] (python)

---
+++ solutions/Hard/2858-minimum-edge-reversals-so-every-node-is-reachable/solution.py
@@ -0,0 +1,43 @@
+# ──────────────────────────────────────────────────
+# Problem  : 2858. Minimum Edge Reversals So Every Node Is Reachable
+# Difficulty: Hard
+# Tags     : Dynamic Programming, Depth-First Search, Breadth-First Search, Graph Theory
+# Link     : https://leetcode.com/problems/minimum-edge-reversals-so-every-node-is-reachable/
+# Runtime  : 0 ms (beats 0%)
+# Memory   : 12436000 (beats 0%)
+# Language : python
+# Copyright: (c) 2026 KANUSHKASELVARAJ. All rights reserved.
+# Synced by: leetie
+# ──────────────────────────────────────────────────
+
+from collections import defaultdict
+
+class Solution:
+    def minEdgeReversals(self, n, edges):
+        adj = defaultdict(list)
+        for u, v in edges:
+            adj[u].append((v, 0)) 
+            adj[v].append((u, 1))  
+
+        ans = [0] * n
+
+       
+        def dfs1(u, p):
+            total = 0
+            for v, cost in adj[u]:
+                if v != p:
+                    total += cost + dfs1(v, u)
+            return total
+
+        ans[0] = dfs1(0, -1)
+
+        
+        def dfs2(u, p):
+            for v, cost in adj[u]:
+                if v != p:
+                   
+                    ans[v] = ans[u] + (1 if cost == 0 else -1)
+                    dfs2(v, u)
+
+        dfs2(0, -1)
+        return ans
