8a96098aee2e47b48ae5036354e0879030e985d3
KANUSHKASELVARAJ <kanushkaselvaraj@users.noreply.github.com>
2026-08-27 15:07:24 +0000

    leetie: Add 74. Search a 2D Matrix [Medium] (python)

--- solutions/Medium/0074-search-a-2d-matrix/solution.py
+++ solutions/Medium/0074-search-a-2d-matrix/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Binary Search, Matrix
 # Link     : https://leetcode.com/problems/search-a-2d-matrix/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12344000 (beats 0%)
+# Runtime  : 0 ms (beats 100%)
+# Memory   : 12492000 (beats 96%)
 # Language : python
 # Copyright: (c) 2026 KANUSHKASELVARAJ. All rights reserved.
 # Synced by: leetie
