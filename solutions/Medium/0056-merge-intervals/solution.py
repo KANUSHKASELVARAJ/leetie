a4efa8ce9a3e4ee81f334cabe0c74b065774f794
KANUSHKASELVARAJ <kanushkaselvaraj@users.noreply.github.com>
2026-08-27 14:57:14 +0000

    leetie: Add 56. Merge Intervals [Medium] (python)

--- solutions/Medium/0056-merge-intervals/solution.py
+++ solutions/Medium/0056-merge-intervals/solution.py
@@ -3,8 +3,8 @@
 # Difficulty: Medium
 # Tags     : Array, Sorting, Quicksort
 # Link     : https://leetcode.com/problems/merge-intervals/
-# Runtime  : 0 ms (beats 0%)
-# Memory   : 12336000 (beats 0%)
+# Runtime  : 9 ms (beats 71%)
+# Memory   : 15852000 (beats 89%)
 # Language : python
 # Copyright: (c) 2026 KANUSHKASELVARAJ. All rights reserved.
 # Synced by: leetie
